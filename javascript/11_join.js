250a12ea893a61c9eee6555232f72129625110ce
eoekstpal <park.se.mi47@gmail.com>
2024-01-18 16:59:11 +0900

    11 join js

--- javascript/11_join.js
+++ javascript/11_join.js
@@ -24,6 +24,11 @@ form.addEventListener("submit", function(event){
     }
 
     document.body.innerHTML = ""
-    document.write(`<p>${userId}님 환영합니다!</p>`)
+    document.write(`<p><b>${userId}</b>님 환영합니다!</p>`)
+    document.write(`<p>회원 가입 시 입력하신 내역은 다음과 같습니다.</p>`)
+    document.write(`<p><b>아이디:</b> ${userId}</p>`)
+    document.write(`<p><b>이름:</b> ${userName}</p>`)
+    document.write(`<p><b>전화번호:</b> ${userPhone}</p>`)
+    document.write(`<p><b>원하는 직무:</b> ${userPosition}</p>`)
 
 })
