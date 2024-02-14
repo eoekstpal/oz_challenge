57b2efb82b2a73b58925d773bd55696032e1e24c
eoekstpal <park.se.mi47@gmail.com>
2024-02-15 00:10:46 +0900

    react create 기능 생성

---
+++ react-crud-app/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './ExpenseItem.css'
+import { MdEdit } from 'react-icons/md';
+import { MdDelete } from 'react-icons/md';
+
+const ExpenseItem = (props) => {
+  return (
+        <li className='item'>
+            <div className='info'>
+                <span className='expense'>{props.expense.charge}</span>
+                <span className='amount'>{props.expense.amount}</span>
+            </div>
+            <div>
+                <button className='edit-btn'><MdEdit/></button>
+                <button onClick={() => this.props.handleDelete(props.expense.id)} className='clear-btn'><MdDelete/></button>
+            </div>
+        </li>
+  )
+}
+export default ExpenseItem
