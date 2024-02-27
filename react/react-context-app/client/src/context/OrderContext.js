c3c8af5fe64e896214cc11857ee4ce30af8a083b
eoekstpal <park.se.mi47@gmail.com>
2024-02-28 02:15:18 +0900

    orderContext state 업데이트 함수

--- react/react-context-app/client/src/context/OrderContext.js
+++ react/react-context-app/client/src/context/OrderContext.js
@@ -3,14 +3,23 @@ import { createContext, useMemo, useState } from "react";
 const OrderContext = createContext();
 
 export function OrderContextProvider(props) {
-    const [orderCounts, serOrderCounts] = useState({
+    const [orderCounts, setOrderCounts] = useState({
         products: new Map(),
         options: new Map(),
     });
 
     {/*useMemo로 감싸준 이유: state가 업데이트 되면 <OrderContextProvider>로 감싸준 것이 다 리렌더링 되기 때문에 최소화 하기위해*/}
     const value = useMemo(() => {
-        return[{ ...orderCounts }];
+        function updateItemCount(itemName, newItemCount, orderType) {
+            const newOrderCounts = [{ ...orderCounts }];
+
+            const orderCountsMap = orderCounts[orderType];
+            orderCountsMap.set(itemName, parseInt(newItemCount));
+
+            setOrderCounts(newOrderCounts);
+        }
+        
+        return [{ ... orderCounts }, updateItemCount];
     }, [orderCounts]);
 
     return <OrderContext.Provider value={value} {...props} />
