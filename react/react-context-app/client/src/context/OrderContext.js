20beef9c11c3f5350ac15133ad9e47229e4234f6
eoekstpal <park.se.mi47@gmail.com>
2024-02-28 11:59:48 +0900

    상품 count를 이용한 가격 계산

--- react/react-context-app/client/src/context/OrderContext.js
+++ react/react-context-app/client/src/context/OrderContext.js
@@ -1,6 +1,6 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 
-const OrderContext = createContext();
+export const OrderContext = createContext();
 
 export function OrderContextProvider(props) {
     const [orderCounts, setOrderCounts] = useState({
@@ -8,7 +8,37 @@ export function OrderContextProvider(props) {
         options: new Map(),
     });
 
-    {/*useMemo로 감싸준 이유: state가 업데이트 되면 <OrderContextProvider>로 감싸준 것이 다 리렌더링 되기 때문에 최소화 하기위해*/}
+    const [totals, setTotals] = useState({
+        products: 0,
+        options: 0,
+        total: 0
+    })
+
+    const pricePerItem = {
+        products: 1000,
+        options: 500
+    }
+
+    const calculateSubtotal = (orderType, orderCounts) => {
+        let optionCount = 0;
+        for (const count of orderCounts[orderType].values()) {
+            optionCount += count;
+        }
+    }
+
+    useEffect(() => {
+        const producsTotal = calculateSubtotal("products", orderCounts);
+        const optionsTotal = calculateSubtotal("options", orderCounts);
+        const total = producsTotal + optionsTotal;
+        setTotals({
+            products: producsTotal,
+            options: optionsTotal,
+            total
+        })
+    }, [orderCounts])
+
+    {/*useMemo로 감싸준 이유: state가 업데이트 되면 
+    <OrderContextProvider>로 감싸준 것이 다 리렌더링 되기 때문에 이것을 최소화 함*/}
     const value = useMemo(() => {
         function updateItemCount(itemName, newItemCount, orderType) {
             const newOrderCounts = [{ ...orderCounts }];
@@ -19,8 +49,8 @@ export function OrderContextProvider(props) {
             setOrderCounts(newOrderCounts);
         }
         
-        return [{ ... orderCounts }, updateItemCount];
-    }, [orderCounts]);
+        return [{ ... orderCounts, totals }, updateItemCount];
+    }, [orderCounts, totals]);
 
     return <OrderContext.Provider value={value} {...props} />
 }
