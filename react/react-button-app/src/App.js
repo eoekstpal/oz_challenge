cb551e6f6b3d750b9b1ab26c36e0b638187ba613
eoekstpal <park.se.mi47@gmail.com>
2024-03-05 17:37:03 +0900

    fireevent로 +- 기능 넣음

---
+++ react/react-button-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testid="counter">{count}</h3>
+        <button data-testid="minus-button"
+          onClick={() => setCount((prev) => prev - 1)}
+        >
+          -
+        </button>
+        <button data-testid="plus-button"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+            +
+          </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
